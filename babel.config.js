37fe5a132c24ade6cadbac4311f6641db1d707c3
Bruno Alehtxandre <brunoaleh1@gmail.com>
2023-12-07 18:03:04 -0300

    configuração de babel e typescript

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugin: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          alias: {
+            "@components": "./src/components",
+            "@config": "./src/config",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@assets": "./src/assets",
+            "@routes": "./src/routes",
+            "@hooks": "./src/hooks",
+            "@services": "./src/services",
+            "@styles": "./src/styles",
+            "@types": "./src/types",
+          },
+        },
+      ],
+    ],
   };
 };
