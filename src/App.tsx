9f7082979fa111e1e53b6afcf6fbea74f7b49671
Bruno Alehtxandre <brunoaleh1@gmail.com>
2023-12-06 22:03:59 -0300

    estruturando as minhas paginas

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Vamos Começar</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
